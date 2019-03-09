7cb2cabcf54b53728258d652d45fe06d052605c2
Nolan Lawson <nolan@nolanlawson.com>
2019-03-09 11:53:25 -0800

    build

---
+++ dist-modern/components/skins-dot.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Skins from './skins';
+
+export default class SkinsDot extends Skins {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    const { skin, i18n } = this.props;
+    const { opened } = this.state;
+    const skinToneNodes = [];
+
+    for (let skinTone = 1; skinTone <= 6; skinTone++) {
+      const selected = skinTone === skin;
+      skinToneNodes.push(React.createElement(
+        'span',
+        {
+          key: `skin-tone-${skinTone}`,
+          className: `emoji-mart-skin-swatch${selected ? ' selected' : ''}`
+        },
+        React.createElement('span', {
+          onClick: this.handleClick,
+          'data-skin': skinTone,
+          className: `emoji-mart-skin emoji-mart-skin-tone-${skinTone}`
+        })
+      ));
+    }
+
+    return React.createElement(
+      'div',
+      { className: `emoji-mart-skin-swatches${opened ? ' opened' : ''}` },
+      skinToneNodes
+    );
+  }
+}
+
+SkinsDot.propTypes /* remove-proptypes */ = {
+  onChange: PropTypes.func,
+  skin: PropTypes.number.isRequired,
+  i18n: PropTypes.object
+};
+
+SkinsDot.defaultProps = {
+  onChange: () => {}
+};
---
+++ dist/components/skins-dot.js
@@ -0,0 +1,66 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _skins = require('./skins');
+
+var _skins2 = _interopRequireDefault(_skins);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+class SkinsDot extends _skins2.default {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    const { skin, i18n } = this.props;
+    const { opened } = this.state;
+    const skinToneNodes = [];
+
+    for (let skinTone = 1; skinTone <= 6; skinTone++) {
+      const selected = skinTone === skin;
+      skinToneNodes.push(_react2.default.createElement(
+        'span',
+        {
+          key: `skin-tone-${skinTone}`,
+          className: `emoji-mart-skin-swatch${selected ? ' selected' : ''}`
+        },
+        _react2.default.createElement('span', {
+          onClick: this.handleClick,
+          'data-skin': skinTone,
+          className: `emoji-mart-skin emoji-mart-skin-tone-${skinTone}`
+        })
+      ));
+    }
+
+    return _react2.default.createElement(
+      'div',
+      { className: `emoji-mart-skin-swatches${opened ? ' opened' : ''}` },
+      skinToneNodes
+    );
+  }
+}
+
+exports.default = SkinsDot;
+SkinsDot.propTypes /* remove-proptypes */ = {
+  onChange: _propTypes2.default.func,
+  skin: _propTypes2.default.number.isRequired,
+  i18n: _propTypes2.default.object
+};
+
+SkinsDot.defaultProps = {
+  onChange: () => {}
+};
