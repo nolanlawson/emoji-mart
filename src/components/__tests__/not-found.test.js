2eec1f118afef0aae1d9c0328be7fe624036114e
Nolan Lawson <nolan@nolanlawson.com>
2019-03-13 18:31:13 -0700

    try jest

---
+++ src/components/__tests__/not-found.test.js
@@ -0,0 +1,12 @@
+// Link.react.test.js
+import React from 'react';
+import NotFound from '../not-found';
+import renderer from 'react-test-renderer';
+
+test('Link changes the class when hovered', () => {
+    const component = renderer.create(
+        <NotFound></NotFound>,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
