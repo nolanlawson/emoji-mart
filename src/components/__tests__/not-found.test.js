aa8a1fadbaa4e10b06af3142ecad5c07384fc237
Nolan Lawson <nolan@nolanlawson.com>
2019-03-13 18:57:57 -0700

    more progress

--- src/components/__tests__/not-found.test.js
+++ src/components/__tests__/not-found.test.js
@@ -1,11 +1,51 @@
 // Link.react.test.js
-import React from 'react';
-import NotFound from '../not-found';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import NotFound from '../not-found'
+import renderer from 'react-test-renderer'
 
-test('Link changes the class when hovered', () => {
+import appleData from '../../../data/apple'
+
+
+const i18n = {
+    search: 'Search',
+    clear: 'Clear', // Accessible label on "clear" button
+    emojilist: 'List of emoji', // Accessible title for list of emoji
+    notfound: 'No Emoji Found',
+    skintext: 'Choose your default skin tone',
+    categories: {
+        search: 'Search Results',
+        recent: 'Frequently Used',
+        people: 'Smileys & People',
+        nature: 'Animals & Nature',
+        foods: 'Food & Drink',
+        activity: 'Activity',
+        places: 'Travel & Places',
+        objects: 'Objects',
+        symbols: 'Symbols',
+        flags: 'Flags',
+        custom: 'Custom',
+    },
+    categorieslabel: 'Emoji categories', // Accessible title for the list of categories
+    skintones: {
+        1: 'Default Skin Tone',
+        2: 'Light Skin Tone',
+        3: 'Medium-Light Skin Tone',
+        4: 'Medium Skin Tone',
+        5: 'Medium-Dark Skin Tone',
+        6: 'Dark Skin Tone',
+    },
+}
+
+test('Renders NotFound', () => {
+    const emojiProps = {"native":true,"skin":1,"size":24,"set":"apple","sheetSize":64,"forceSize":true,"tooltip":false}
     const component = renderer.create(
-        <NotFound></NotFound>,
+        <NotFound
+            data={appleData}
+            notFound={() => {}}
+    notFoundEmoji={"sleuth_or_spy"}
+    emojiProps={emojiProps}
+            i18n={i18n}
+    />,
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
